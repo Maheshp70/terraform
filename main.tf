9c22f29cbacb785a350fcd76a8176f28d0fea13a
Mahesh <pmaheshgoud222@gmail.com>
2023-12-15 12:12:00 +0530

    added main file

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+    region = "ap-south-1"
+
+}
+resource "aws_vpc" "first" {
+  cidr_block = 192.168.0.0/16
+}
